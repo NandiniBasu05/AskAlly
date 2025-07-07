dfefc2490a92179804ae2266d5e0ba5f459e7d79
NandiniBasu05 <nandinibasu240105@gmail.com>
2025-07-07 17:45:08 +0530

    Fixed: wrong import

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,13 +1,12 @@
 import { getIndustryInsights } from "@/actions/dashboard";
-import DashboardView from "./_component/dashboard-view";
+import DashboardView from "./_components/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
   const { isOnboarded } = await getUserOnboardingStatus();
 
-  // If not onboarded, redirect to onboarding page
-  // Skip this check if already on the onboarding page
+  
   if (!isOnboarded) {
     redirect("/onboarding");
   }
