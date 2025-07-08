6e476fcf76c8235f27abc5791d4121751b56e0c4
NandiniBasu05 <nandinibasu240105@gmail.com>
2025-07-08 15:13:01 +0530

    Fixed: testimonial pictures

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
-  },
+    
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "randomuser.me",
+                 pathname: "/api/portraits/**",
+            }
+        ]
+    },
+
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "5mb",
+        },
+    },
 };
 
 export default nextConfig;
